1e6c2dbffd4bcd9233f2095a22ff426ef2eaa414
Mr. Hasan <hasanshahriar32@gmail.com>
2023-09-23 02:13:12 +0600

    updated api works

---
+++ src/pages/api/products/[id]/index.js
@@ -0,0 +1,44 @@
+// export default function handler(req, res) {
+//   const { pid } = req.query;
+//   res.end(`Post: ${pid}`);
+// }
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function dbConnection(req, res) {
+  try {
+    await client.connect();
+    console.log("mongo connected successfully ");
+    const categoryCollection = client.db("paradox").collection("product");
+    const { id } = req.query;
+    if (req.method === "GET") {
+      console.log(id);
+      const news = await categoryCollection.findOne({
+        category_id: parseInt(id),
+      });
+      res.send({ message: "success", status: 200, data: news });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal server error", status: 500 });
+  } finally {
+    await client.close();
+  }
+}
+export default dbConnection;
---
+++ src/pages/api/products/index.js
@@ -0,0 +1,32 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function dbConnection(req, res) {
+  try {
+    await client.connect();
+    console.log("mongo connected successfully ");
+    const categoryCollection = client.db("paradox").collection("product");
+    if (req.method === "GET") {
+      const news = await categoryCollection.find({}).toArray();
+      res.send({ message: "success", status: 200, data: news });
+    }
+  } finally {
+    await client.close();
+  }
+}
+export default dbConnection;
